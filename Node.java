1f58d95bfb0d6b9f9542f89b2d03aec5ba8fba9a
ctshara14 <ctsharath14@gmail.com>
2022-08-08 16:13:12 +0530

    Adding: Program to create Stacks

---
+++ Node.java
@@ -0,0 +1,11 @@
+public class Node {
+    public int data;
+    public Node next;
+
+   // data - values defined in nodes
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
