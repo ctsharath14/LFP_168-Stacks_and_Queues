2957095f0929207f8c6e02ebac94590354ef5b7d
ctshara14 <ctsharath14@gmail.com>
2022-08-08 16:29:29 +0530

    Adding: Program to Peak and Pop the elements of Stack

--- StackMain.java
+++ StackMain.java
@@ -1,11 +1,17 @@
 public class StackMain {
-
     public static void main(String[] args) {
 
         StackService list = new StackService();
         list.push(70);
         list.push(30);
         list.push(56);
+        list.peak();
+        list.pop();
+        list.peak();
+        list.pop();
+        list.peak();
+        list.pop();
+        list.peak();
         list.printList();
     }
 }
